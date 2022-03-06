191784e2d15bd54ef605e403aca7065020be3370
Stuuu <stuartschrick@gmail.com>
2022-03-06 08:30:38 -0500

    344.ReverseString.php

---
+++ 344.ReverseString.php
@@ -0,0 +1,39 @@
+<?php
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+// Accepted
+// 1,431,301
+// Submissions
+class Solution
+{
+
+    /**
+     * @param String[] $s
+     * @return NULL
+     */
+    function reverseString(&$s)
+    {
+        return $s = array_reverse($s);
+    }
+}
+$s = ["h", "e", "l", "l", "o"];
+// Output: ["o","l","l","e","h"]
+(new Solution())->reverseString($s);
