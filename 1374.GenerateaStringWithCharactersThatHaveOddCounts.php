7175e779c00b6dcfcac265c33153fae06047d55c
Stuuu <stuartschrick@gmail.com>
2022-04-01 08:22:40 -0400

    1374.GenerateaStringWithCharactersThatHaveOddCounts.php

---
+++ 1374.GenerateaStringWithCharactersThatHaveOddCounts.php
@@ -0,0 +1,43 @@
+<?php
+// Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+
+// The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.  
+
+
+
+// Example 1:
+
+$n = 4;
+// Output: "pppz"
+// Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+// Example 2:
+
+// $n = 2;
+// Output: "xy"
+// Explanation: "xy" is a valid string since the characters 'x' and 'y' occur once. Note that there are many other valid strings such as "ag" and "ur".
+// Example 3:
+
+$n = 7;
+// Output: "holasss"
+
+
+// Constraints:
+
+// 1 <= n <= 500
+class Solution
+{
+
+    /**
+     * @param int $n
+     * @return String
+     */
+    function generateTheString($n)
+    {
+        if ($n === 1) return 'x';
+        if (($n % 2) == 0) {
+            return  str_repeat('x', $n - 1) . 'y';
+        }
+        return str_repeat('x', $n - 2) . 'y' . 'z';
+    }
+}
+echo (new Solution())->generateTheString($n);
