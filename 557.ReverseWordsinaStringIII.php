b75fa053a84343281df7343f6e2c84c84c86ef1f
Stuuu <stuartschrick@gmail.com>
2022-04-16 21:48:13 -0400

    557.ReverseWordsinaStringIII.php

---
+++ 557.ReverseWordsinaStringIII.php
@@ -0,0 +1,35 @@
+<?php
+// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+// Example 1:
+
+$s = "Let's take LeetCode contest";
+// Output: "s'teL ekat edoCteeL tsetnoc"
+// Example 2:
+
+// $s = "God Ding";
+// Output: "doG gniD"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 5 * 104
+// s contains printable ASCII characters.
+// s does not contain any leading or trailing spaces.
+// There is at least one word in s.
+// All the words in s are separated by a single space.
+class Solution
+{
+
+  /**
+   * @param String $s
+   * @return String
+   */
+    public function reverseWords($s)
+    {
+        return implode(' ', array_map('strrev', explode(' ', $s)));
+    }
+}
+echo (new Solution())->reverseWords($s);
