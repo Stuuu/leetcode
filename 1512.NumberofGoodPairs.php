e02cfef202dd4c5537c860ace48a77715e12b5ff
Stuuu <stuartschrick@gmail.com>
2022-11-06 18:28:52 -0500

    1512.NumberofGoodPairs.php

---
+++ 1512.NumberofGoodPairs.php
@@ -0,0 +1,48 @@
+<?php
+// Given an array of integers nums, return the number of good pairs.
+
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+// Example 2:
+
+// Input: nums = [1,1,1,1]
+// Output: 6
+// Explanation: Each pair in the array are good.
+// Example 3:
+
+$nums = [1,2,3];
+// Output: 0
+
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function numIdenticalPairs($nums)
+    {
+        $pairs = 0;
+        foreach($nums as $k => $num){
+            $larger_slice = array_slice($nums, $k);
+            $pairs += count(array_keys($larger_slice, $num)) - 1;
+        }
+        return $pairs;
+    }
+}
+// $nums = [1, 2, 3, 1, 1, 3];
+// Output: 4
+// $nums = [1, 1, 1, 1];
+// Output: 6
+echo (new Solution())->numIdenticalPairs($nums);
