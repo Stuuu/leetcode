9809e3b18b7a671cb020e14fef69996acd20a920
Stuuu <stuartschrick@gmail.com>
2022-12-04 19:09:10 -0500

    2490.CircularSentence.php

---
+++ 2490.CircularSentence.php
@@ -0,0 +1,71 @@
+<?php
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+// For example, "Hello World", "HELLO", "hello world hello world" are all sentences.
+// Words consist of only uppercase and lowercase English letters. Uppercase and lowercase English letters are considered different.
+
+// A sentence is circular if:
+
+// The last character of a word is equal to the first character of the next word.
+// The last character of the last word is equal to the first character of the first word.
+// For example, "leetcode exercises sound delightful", "eetcode", "leetcode eats soul" are all circular sentences. However, "Leetcode is cool", "happy Leetcode", "Leetcode" and "I like Leetcode" are not circular sentences.
+
+// Given a string sentence, return true if it is circular. Otherwise, return false.
+
+
+
+// Example 1:
+
+$sentence = "leetcode exercises sound delightful";
+// Output: true
+// Explanation: The words in sentence are ["leetcode", "exercises", "sound", "delightful"].
+// - leetcode's last character is equal to exercises's first character.
+// - exercises's last character is equal to sound's first character.
+// - sound's last character is equal to delightful's first character.
+// - delightful's last character is equal to leetcode's first character.
+// The sentence is circular.
+// Example 2:
+
+$sentence = "eetcode";
+// Output: true
+// Explanation: The words in sentence are ["eetcode"].
+// - eetcode's last character is equal to eetcode's first character.
+// The sentence is circular.
+// Example 3:
+
+$sentence = "Leetcode is cool";
+// Output: false
+// Explanation: The words in sentence are ["Leetcode", "is", "cool"].
+// - Leetcode's last character is not equal to is's first character.
+// The sentence is not circular.
+class Solution
+{
+
+  /**
+   * @param String $sentence
+   * @return Boolean
+   */
+    public function isCircularSentence($sentence)
+    {
+        // The last character of the last word is equal to the first character of the first word.
+        if ($sentence[0] !== substr($sentence, -1)) {
+            return false;
+        };
+
+
+        // The last character of a word is equal to the first character of the next word.
+        $words = explode(' ', $sentence);
+        foreach ($words as $k => $word) {
+            if (!isset($words[$k + 1])) {
+                break;
+            }
+            $last_letter = substr($word, -1);
+            $next_first_letter = $words[$k + 1][0];
+            if ($next_first_letter !== $last_letter) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+var_dump((new Solution())->isCircularSentence($sentence));
