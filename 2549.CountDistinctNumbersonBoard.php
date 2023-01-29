30151b4b31ecb3db5b14572711f5d74753946046
Stuuu <stuartschrick@gmail.com>
2023-01-29 07:54:56 -0500

    2549.CountDistinctNumbersonBoard.php

---
+++ 2549.CountDistinctNumbersonBoard.php
@@ -0,0 +1,61 @@
+<?php
+// You are given a positive integer n, that is initially placed on a board. Every day, for 109 days, you perform the following procedure:
+
+//   For each number x present on the board, find all numbers 1 <= i <= n such that x % i == 1.
+//   Then, place those numbers on the board.
+//   Return the number of distinct integers present on the board after 109 days have elapsed.
+
+//   Note:
+
+//   Once a number is placed on the board, it will remain on it until the end.
+//   % stands for the modulo operation. For example, 14 % 3 is 2.
+
+
+//   Example 1:
+
+$n = 5;
+//   Output: 4
+//   Explanation: Initially, 5 is present on the board.
+//   The next day, 2 and 4 will be added since 5 % 2 == 1 and 5 % 4 == 1.
+//   After that day, 3 will be added to the board because 4 % 3 == 1.
+//   At the end of a billion days, the distinct numbers on the board will be 2, 3, 4, and 5.
+//   Example 2:
+
+// $n = 3;
+//   Output: 2
+//   Explanation:
+//   Since 3 % 2 == 1, 2 will be added to the board.
+//   After a billion days, the only two distinct numbers on the board are 2 and 3.
+
+
+//   Constraints:
+
+//   1 <= n <= 100
+class Solution
+{
+
+  /**
+   * @param int $n
+   * @return int
+   */
+  public function distinctIntegers($n)
+  {
+    $board[$n] = 1;
+    for ($days = 0; $days < 109; $days++) {
+
+      foreach ($board as $board_item => $thowaway_val) {
+
+        for ($i=1; $i < $board_item; $i++) {
+
+          if($board_item % $i === 1 ){
+            $board[$i] = 1;
+          }
+
+        }
+
+      }
+    }
+    return count($board);
+  }
+}
+(new Solution())->distinctIntegers($n);
