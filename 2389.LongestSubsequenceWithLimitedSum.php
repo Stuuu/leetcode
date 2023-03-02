d367521ae54e7e6dd5e569c6a05beab4b531cb0f
Stuuu <stuartschrick@gmail.com>
2023-03-02 07:09:02 -0500

    2389.LongestSubsequenceWithLimitedSum.php

---
+++ 2389.LongestSubsequenceWithLimitedSum.php
@@ -0,0 +1,60 @@
+<?php
+// You are given an integer array nums of length n, and an integer array queries of length m.
+
+// Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
+
+// A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+
+
+// Example 1:
+
+$nums = [4, 5, 2, 1];
+$queries = [3, 10, 21];
+// Output: [2,3,4]
+// Explanation: We answer the queries as follows:
+// - The subsequence [2,1] has a sum less than or equal to 3. It can be proven that 2 is the maximum size of such a subsequence, so answer[0] = 2.
+// - The subsequence [4,5,1] has a sum less than or equal to 10. It can be proven that 3 is the maximum size of such a subsequence, so answer[1] = 3.
+// - The subsequence [4,5,2,1] has a sum less than or equal to 21. It can be proven that 4 is the maximum size of such a subsequence, so answer[2] = 4.
+// Example 2:
+
+$nums = [2, 3, 4, 5];
+$queries = [1];
+// Output: [0]
+// Explanation: The empty subsequence is the only subsequence that has a sum less than or equal to 1, so answer[0] = 0.
+
+
+// Constraints:
+
+// n == nums.length
+// m == queries.length
+// 1 <= n, m <= 1000
+// 1 <= nums[i], queries[i] <= 106
+class Solution
+{
+
+  /**
+   * @param int[] $nums
+   * @param int[] $queries
+   * @return int[]
+   */
+  public function answerQueries($nums, $queries)
+  {
+    sort($nums);
+    $ans = [];
+    foreach ($queries as $k => $max_sum) {
+      $ans[$k] = 0;
+      $cur_sum = 0;
+      foreach ($nums as $k2 => $num) {
+        $cur_sum += $num;
+        if (($cur_sum) <= $max_sum) {
+          $ans[$k]++;
+          continue;
+        }
+        break;
+      }
+    }
+    return $ans;
+  }
+}
+(new Solution())->answerQueries($nums, $queries);
