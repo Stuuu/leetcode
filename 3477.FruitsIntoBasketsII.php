8814d150f13d29079df68eaeaddea792152eed51
stu <stuartschrick@gmail.com>
2025-12-07 20:39:14 -0500

    3477.FruitsIntoBasketsII.php

---
+++ 3477.FruitsIntoBasketsII.php
@@ -0,0 +1,73 @@
+<?php
+
+// You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+
+// From left to right, place the fruits according to these rules:
+
+// Each fruit type must be placed in the leftmost available basket with a capacity greater than or equal to the quantity of that fruit type.
+// Each basket can hold only one type of fruit.
+// If a fruit type cannot be placed in any basket, it remains unplaced.
+// Return the number of fruit types that remain unplaced after all possible allocations are made.
+
+
+
+// Example 1:
+
+$fruits = [4, 2, 5];
+$baskets = [3, 5, 4];
+
+// Output: 1
+
+// Explanation:
+
+// fruits[0] = 4 is placed in baskets[1] = 5.
+// fruits[1] = 2 is placed in baskets[0] = 3.
+// fruits[2] = 5 cannot be placed in baskets[2] = 4.
+// Since one fruit type remains unplaced, we return 1.
+
+// Example 2:
+
+// $fruits = [3,6,1];
+// $baskets = [6,4,7];
+
+// Output: 0
+
+// Explanation:
+
+// fruits[0] = 3 is placed in baskets[0] = 6.
+// fruits[1] = 6 cannot be placed in baskets[1] = 4 (insufficient capacity) but can be placed in the next available basket, baskets[2] = 7.
+// fruits[2] = 1 is placed in baskets[1] = 4.
+// Since all fruits are successfully placed, we return 0.
+
+
+
+// Constraints:
+
+// n == fruits.length == baskets.length
+// 1 <= n <= 100
+// 1 <= fruits[i], baskets[i] <= 1000
+
+class Solution
+{
+
+	/**
+	 * @param int[] $fruits
+	 * @param int[] $baskets
+	 * @return int
+	 */
+	function numOfUnplacedFruits($fruits, $baskets)
+	{
+		foreach ($fruits as $k => $fruit_count) {
+			foreach ($baskets as $k2 => $basket) {
+				if ($fruit_count <= $basket) {
+					unset($baskets[$k2]);
+					unset($fruits[$k]);
+					break;
+				}
+			}
+		}
+		return count($fruits);
+	}
+}
+
+print_r((new Solution())->numOfUnplacedFruits($fruits, $baskets));
